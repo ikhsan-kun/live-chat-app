96b12ad0943782abb4eec3337d8d039a8f64ee2c
Ikhsan-kun <xsanofficial@gmail.com>
2025-07-17 21:32:24 +0700

    components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { checkAuthenticatedRoute } from '../utils/auth';
+
 function Home() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    checkAuthenticatedRoute(navigate);
+  }, [navigate]);
+  
   return <h1>Halaman Home</h1>;
 }
 export default Home;
